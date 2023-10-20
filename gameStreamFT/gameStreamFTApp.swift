9194475880cea2fdff30b9d1d921b7b8a3694614
Fabian <fabianu2t@gmail.com>
2023-10-20 09:58:20 -0300

    Initial Commit

---
+++ gameStreamFT/gameStreamFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameStreamFTApp.swift
+//  gameStreamFT
+//
+//  Created by Fabian on 20-10-23.
+//
+
+import SwiftUI
+
+@main
+struct gameStreamFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
